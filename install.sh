c053e06afdcbdd38eaf78d5f1293cd003e547607
Raphael Serafim <rapahelvserafim@gmail.com>
2024-01-25 17:29:46 +0100

    feat: upgrade

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 sudo apt-get update -y
 sudo apt-get install python3-pip -y
 pip3 install -r requirements.txt
+pip install --upgrade Flask
+pip install --upgrade Werkzeug
 python3 run.py
 
 #sudo lsof -i :3000
