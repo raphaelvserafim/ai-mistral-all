d479878eb5aa043c96738ca1e6c0c4dc06985b24
Raphael Serafim <raphaelvserafim@gmail.com>
2025-05-01 15:00:30 -0400

    fix: update install script and requirements for package compatibility

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo apt-get update -y
 sudo apt-get install python3-pip -y
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 pip install --upgrade Flask
 pip install --upgrade Werkzeug
 pip install flask[async]
